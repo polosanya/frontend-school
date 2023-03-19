a6fbd330997873b1834fe4ebe84acd88ab1325d3
Oleksandr Polotniuk <oleksandr.polotniuk@gmail.com>
2023-03-20 01:32:17 +0200

    video speed choose feature added

--- src/components/VideoPlayer/VideoPlayer.tsx
+++ src/components/VideoPlayer/VideoPlayer.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import ReactHlsPlayer from "react-hls-player";
 import { useLocalStorage } from "../../helpers/useLocalStorage";
 
@@ -9,21 +9,12 @@ type Props = {
 const VideoPlayer: React.FC<Props> = ({ link }) => {
   const playerRef = useRef<HTMLVideoElement>(null);
   const [videoProgress, setVideoProgress] = useLocalStorage<number>(link, 0);
+  const [speed, setSpeed] = useState<number>(1);
 
   useEffect(() => {
     if (videoProgress !== 0 && playerRef.current) {
       playerRef.current.currentTime = videoProgress;
     }
-
-    const handleBeforeUnload = () => {
-      if (playerRef.current) {
-        const newTime = playerRef.current.currentTime;
-
-        if (newTime !== 0 && newTime !== videoProgress) {
-          setVideoProgress(newTime);
-        }
-      }
-    };
 
     const handleUnload = () => {
       if (playerRef.current) {
@@ -35,24 +26,55 @@ const VideoPlayer: React.FC<Props> = ({ link }) => {
       }
     };
 
-    window.addEventListener("beforeunload", handleBeforeUnload);
-    window.addEventListener("unload", handleUnload);
-
-    return () => {
-      window.removeEventListener("beforeunload", handleBeforeUnload);
-      window.removeEventListener("unload", handleUnload);
-    };
+    return handleUnload;
   }, [playerRef, setVideoProgress, videoProgress, link]);
 
+  useEffect(() => {
+    if (playerRef.current) {
+      playerRef.current.playbackRate = speed;
+    }
+  }, [speed])
+
+  useEffect(() => {
+    const handleKeyUp = (event: KeyboardEvent) => {
+      if (event.key === "+") {
+        setSpeed(prevSpeed => (
+          prevSpeed !== 2
+            ? prevSpeed + 0.25
+            : prevSpeed
+        ));
+      }
+  
+      if (event.key === "-") {
+        setSpeed(prevSpeed => (
+          prevSpeed !== 0.25
+            ? prevSpeed - 0.25
+            : prevSpeed
+        ));
+      }
+    };
+
+    window.addEventListener("keyup", handleKeyUp);
+
+    return () => {
+      window.addEventListener("keyup", handleKeyUp);
+    };
+  }, []);
+
   return (
-    <ReactHlsPlayer
-      src={link}
-      autoPlay={false}
-      controls={true}
-      width="60%"
-      height="auto"
-      playerRef={playerRef}
-    />
+    <>
+      <ReactHlsPlayer
+        src={link}
+        autoPlay={false}
+        controls={true}
+        width="60%"
+        height="auto"
+        playerRef={playerRef}
+      />
+
+      <p>You can change video speed by + and - keys on your NumPad</p>
+      <i>Current speed: {speed.toFixed(2)}</i>
+    </>
   );
 };
 
